3243e8fc38a8df8247c4281add2dd5e8db297d05
Katherine Bayard <katherinebayard@zipcoders-MacBook-Pro-6.local>
2019-02-05 18:04:18 -0500

    final Alice and Bob Lab files

--- Main.java
+++ Main.java
@@ -1,9 +1,18 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+    /**
+    * Created by iyasuwatts on 10/17/17.
+    */
+import java.util.Scanner;
 public class Main {
-
-    public static void main(String[] args ){
-
-    }
+    
+  public static void main(String[] args ){
+     Scanner in = new Scanner(System.in);
+            
+     System.out.print("What is your first name? ");
+     
+     String firstName = in.nextLine(); 
+     
+     if(firstName.equals("Alice") || firstName.equals("Bob")) {
+            System.out.println("Hi " + firstName); 
+        }
+  }
 }
